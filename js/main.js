a72f4c3cc4662c0805f02e42d36528ae8778f4b9
omatoro <okayama@grincode.com>
2013-06-10 20:42:28 +0900

    画面幅修正

--- js/main.js
+++ js/main.js
@@ -4,8 +4,11 @@
 var game = game || {};
 
 var SCREEN_WIDTH  = 960;
-var SCREEN_HEIGHT = 680;
+var SCREEN_HEIGHT = 640;
 
+/**
+ * リソースの読み込み
+ */
 var ASSETS = {
     "player":   "http://rawgithub.com/omatoro/tmlib.js_tutorial_avoidgame/master/EX_音を鳴らそう/02_SE/rsc/[Animal]Chicken.png",
     "playerSS": "http://rawgithub.com/omatoro/tmlib.js_tutorial_avoidgame/master/EX_音を鳴らそう/02_SE/rsc/playerSS.tmss",
@@ -13,14 +16,6 @@ var ASSETS = {
     "bgm":      "http://rawgithub.com/omatoro/tmlib.js_tutorial_avoidgame/master/EX_音を鳴らそう/02_SE/rsc/Comical01_Koya_short2.mp3",
     "backMap":  "http://rawgithub.com/omatoro/tmlib.js_tutorial_avoidgame/master/EX_音を鳴らそう/02_SE/rsc/map.png",
 };
-/**
- * リソースの読み込み
- */
-// tm.preload(function() {
-//     // 音
-//     tm.sound.SoundManager.add("bgm", "http://rawgithub.com/omatoro/tmlib.js_tutorial_TouchGame/master/EX_%E9%9F%B3%E3%82%92%E9%B3%B4%E3%82%89%E3%81%9D%E3%81%86/02_SE/rsc/Comical01_Koya.mp3");
-//     tm.sound.SoundManager.add("se",  "http://rawgithub.com/omatoro/tmlib.js_tutorial_TouchGame/master/EX_%E9%9F%B3%E3%82%92%E9%B3%B4%E3%82%89%E3%81%9D%E3%81%86/02_SE/rsc/%5BSystem%5DCursor03_panop.ogg");
-// });
 
 /**
  * ゲーム起動処理
