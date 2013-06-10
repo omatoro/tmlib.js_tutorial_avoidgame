cb2e2f23315c5e5e9bd31671b2c99b7fa8d8eed9
omatoro <okayama@grincode.com>
2013-06-10 20:27:40 +0900

    gh-pages用

--- EX_音を鳴らそう/01_BGM/js/resource.js
+++
@@ -1,9 +0,0 @@
-/**
- * リソースの読み込み
- */
-tm.preload(function() {
-
-	// 音
-	tm.sound.SoundManager.add("bgm", "http://rawgithub.com/omatoro/tmlib.js_tutorial_TouchGame/master/EX_%E9%9F%B3%E3%82%92%E9%B3%B4%E3%82%89%E3%81%9D%E3%81%86/01_BGM/rsc/Comical01_Koya.mp3");
-
-});
